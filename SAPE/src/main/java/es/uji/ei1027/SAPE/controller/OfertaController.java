7741ad94ea6626ee8a01d9a36dbc602365ce6d30
gema <gema@gema-pc>
2018-04-10 19:20:34 +0200

    bla

--- SAPE/src/main/java/es/uji/ei1027/SAPE/controller/OfertaController.java
+++ SAPE/src/main/java/es/uji/ei1027/SAPE/controller/OfertaController.java
@@ -1,4 +1,4 @@
-package es.uji.ei1027.SAPE.controller;
+/*package es.uji.ei1027.SAPE.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -77,4 +77,4 @@ public class OfertaController {
 		
 		// public String estadoOferta
 
-}
+}*/
