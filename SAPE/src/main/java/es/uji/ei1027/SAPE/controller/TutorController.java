df9c636ed43f69e128904382d0caa256ac04d101
al315684 <al315684@alib0027act.sg.uji.es>
2018-04-26 20:13:00 +0200

    Controlador de tutor modificado

--- SAPE/src/main/java/es/uji/ei1027/SAPE/controller/TutorController.java
+++ SAPE/src/main/java/es/uji/ei1027/SAPE/controller/TutorController.java
@@ -1,19 +1,14 @@
 package es.uji.ei1027.SAPE.controller;
-import java.util.Map;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import es.uji.ei1027.SAPE.dao.DaoTutor;
 import es.uji.ei1027.SAPE.model.Personal;
-import es.uji.ei1027.SAPE.model.Tutor;
 
 
 @Controller
