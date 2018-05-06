529c4329e3578c00b10987e55d2fe9f038986a47
Largom <lgcworker@gmail.com>
2018-05-06 10:54:36 +0200

    # WARNING: head commit changed in the meantime
    Controlador estudiante actualizado

--- SAPE/src/main/java/es/uji/ei1027/SAPE/controller/OfertaController.java
+++ SAPE/src/main/java/es/uji/ei1027/SAPE/controller/OfertaController.java
@@ -9,16 +9,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import es.uji.ei1027.SAPE.model.Asignacion;
 import es.uji.ei1027.SAPE.model.EstadoOferta;
 import es.uji.ei1027.SAPE.model.Estancia;
 import es.uji.ei1027.SAPE.model.OfertaProyecto;
 import es.uji.ei1027.SAPE.model.Personal;
 import es.uji.ei1027.SAPE.dao.DaoOfertaProyecto;
+
+
+class OfertaValidator implements Validator { 
+	
+    @Override
+    public boolean supports(Class<?> cls) { 
+        return OfertaProyecto.class.isAssignableFrom(cls);
+    }
+    @Override 
+    public void validate(Object obj, Errors errors) {
+    	OfertaProyecto  oferta = (OfertaProyecto)obj;
+    	if (oferta.getTarea().equals(""))
+		       errors.rejectValue("tarea", "obligatori", "Es necesario introducir una tarea");
+    	if (oferta.getObjetivo().equals(""))
+		       errors.rejectValue("objetivo", "obligatori", "Es necesario introducir una objetivo");
+    	if (oferta.getItinerarios().equals(""))
+		       errors.rejectValue("itinerarios", "obligatori", "Es necesario introducir un o varios itinerarios");
+    }
+    
+    
+}
+
+
+
 
 
 @Controller
@@ -32,14 +59,6 @@ public class OfertaController {
 		this.ofertaProyectoDao = oferta;
 	}
 	
-	
-
-	Map<Short, OfertaProyecto> getOfertasProyectos(final String usu, final String pass);
-	OfertaProyecto getOfertaProyecto(final String usu, final String pass, final short id);
-	boolean crearEditarOfertaProyecto(final String usu, final String pass, final OfertaProyecto ofePro);
-	
-	boolean anularOfertaProyecto(final String usu, final String pass, final short id);
-	boolean editarEstadoOfertaProyecto(final String usu, final String pass, final short id, final EstadoOferta estado);
 		
 	
 	@RequestMapping("/list.html")
@@ -87,11 +106,93 @@ public class OfertaController {
 		return "oferta/list";
 	}
 	
+	@RequestMapping(value="/add/{id}")
+	public String nuevaOferta(HttpSession session, Model model, @PathVariable short id) {
+		
+		Personal usuario = (Personal) session.getAttribute("user");
+		if(usuario == null) {
+			session.setAttribute("nextUrl", "/oferta/add.html");
+			model.addAttribute("user", new String());
+			model.addAttribute("pass", new String());
+			return "login";
+		}
+		
+		model.addAttribute("nuevaOferta", new OfertaProyecto(id));
+		return "oferta/add";
+	}
+	
+	 
+    @RequestMapping(value="/add/{id}", method=RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("nuevaOferta") OfertaProyecto ofertaNueva, BindingResult bindingResult, HttpSession session, Model model) {
+    	
+    	Personal usuario = (Personal) session.getAttribute("user");
+		if(usuario == null) {
+			session.setAttribute("nextUrl", "/oferta/add.html");
+			model.addAttribute("user", new String());
+			model.addAttribute("pass", new String());
+			return "login";
+		}
+    		
+    	OfertaValidator ofertaValidator = new OfertaValidator(); 
+    	ofertaValidator.validate(ofertaNueva, bindingResult);  
+    	if (bindingResult.hasErrors()) 
+                return "oferta/add";
+         ofertaProyectoDao.crearEditarOfertaProyecto(usuario.getUsuario(), usuario.getPass(), ofertaNueva);
+         return "redirect:list.html"; 
+     }
 	
 	
 	
+	 
 	
+	@RequestMapping(value="/editar/{id}", method = RequestMethod.GET)
+	public String editarOferta(HttpSession session, Model model, @PathVariable short id) {
+		
+		Personal usuario = (Personal) session.getAttribute("user");
+		if(usuario == null) {
+			session.setAttribute("nextUrl", "/asignacion/list.html");
+			model.addAttribute("user", new String());
+			model.addAttribute("pass", new String());
+			return "login";
+		}
+		
+		model.addAttribute("ofertaEditar", ofertaProyectoDao.getOfertaProyecto(usuario.getUsuario(), usuario.getPass(),id));
+		return "oferta/list";
+	}
 	
+	@RequestMapping(value="/editar/{id}", method = RequestMethod.POST)
+	public String  procesoUpdateSubmit(HttpSession session, Model model, @PathVariable short id, @ModelAttribute("ofertaEditar") OfertaProyecto oferta, BindingResult bindingResult) {
+		
+		Personal usuario = (Personal) session.getAttribute("user");
+		if(usuario == null) {
+			session.setAttribute("nextUrl", "oferta/editar.html");
+			model.addAttribute("user", new String());
+			model.addAttribute("pass", new String());
+			return "login";
+		}
+		if (bindingResult.hasErrors()) 
+            return "oferta/editar/{id}";
+		
+		ofertaProyectoDao.crearEditarOfertaProyecto(usuario.getUsuario(), usuario.getPass(),oferta);
+		return "oferta/list";
+	}
+	
+	@RequestMapping(value="/editar/{id, estado}")
+	public String  editarEstado(HttpSession session, Model model, @PathVariable short id, @PathVariable EstadoOferta estado) {
+		
+		Personal usuario = (Personal) session.getAttribute("user");
+		if(usuario == null) {
+			session.setAttribute("nextUrl", "/oferta/editar.html");
+			model.addAttribute("user", new String());
+			model.addAttribute("pass", new String());
+			return "login";
+		}
+		
+		ofertaProyectoDao.editarEstadoOfertaProyecto(usuario.getUsuario(), usuario.getPass(),id, estado);
+		return "oferta/list";
+	}
+	
+}	
 	
 /**	
 	@RequestMapping("/list") 
@@ -149,5 +250,5 @@ public class OfertaController {
 
 }*/
 
-}
+
 
